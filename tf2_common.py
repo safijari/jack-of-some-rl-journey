b1a88d4efeca808f07c0ab18ea5821ae515db935
Jariullah Safi <safijari@isu.edu>
2020-03-02 19:56:55 -0800

    a2c again

--- tf2_common.py
+++ tf2_common.py
@@ -2,15 +2,18 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.keras import Model
 
-def make_main_model(input_shape, num_actions):
-    return tf.keras.Sequential(
-        [
+def make_main_model(input_shape, num_actions, include_finals=True):
+    layers = [
             Conv2D(32, 8, padding='valid', activation='relu', strides=(4, 4), input_shape=input_shape),
             Conv2D(64, 4, padding='valid', activation='relu', strides=(2, 2)),
             Conv2D(64, 3, padding='valid', activation='relu', strides=(1, 1)),
             Flatten(),
-            Dense(512, activation='relu'),
-            Dense(num_actions),
-        ])
+        ]
+    if include_finals:
+        d1 = Dense(512, activation='relu')
+        d = Dense(num_actions)
+        layers.append(d1)
+        layers.append(d)
+    return tf.keras.Sequential(layers)
 
 # taken from https://github.com/openai/baselines/blob/tf2/baselines/deepq/deepq_learner.py
